cdc1bf0aae678c5b9faf2c7bd6aa56599e069ac9
Soheil <soheilfeizi@hotmail..com>
2024-09-22 17:21:11 +0330

    update app.js

--- src/App.js
+++ src/App.js
@@ -10,39 +10,12 @@ const SESSION_TIMEOUT = 15 * 60 * 1000; // 15 minutes in milliseconds
 
 function App() {
   const router = useRoutes(routes);
-  const location = useLocation();
-
-  const [isLogin, setIsLogin] = useState(() => {
-    const loginData = localStorage.getItem('loginData');
-    if (loginData) {
-      const { timestamp } = JSON.parse(loginData);
-      if (Date.now() - timestamp < SESSION_TIMEOUT) {
-        return true;
-      }
-    }
-    return false;
-  });
-
-  const isLoginRoute = location.pathname === '/login';
-
-  useEffect(() => {
-    if (isLogin) {
-      const loginData = {
-        timestamp: Date.now()
-      };
-      localStorage.setItem('loginData', JSON.stringify(loginData));
-    }
-  }, [isLogin]);
-
-  if (!isLogin) {
-    return <LoginForm setIsLogin={setIsLogin} />;
-  }
 
   return (
     <>
-      {!isLoginRoute && <NavBar />}
+       <NavBar />
       {router}
-      {!isLoginRoute && <Footer />}
+      <Footer />
     </>
   );
 }
