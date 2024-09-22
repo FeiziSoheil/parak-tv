56cb309ea83b61222b40f98d1ca049c58fae2b6a
Soheil <soheilfeizi@hotmail..com>
2024-09-23 01:06:15 +0330

    update src

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import Header from "./assets/Components/Header/Header";
 import MoviesList from "./assets/Components/MoviesList/MoviesList";
 import MainMovie from "./assets/Pages/MainMovie/MainMovie";
-import LoginForm from "./assets/Pages/Login-form/LoginForm";
+
 
 let routes = [
     { path: "/", element: <MoviesList /> },
     { path: "/movie/:mainMovieID", element: <MainMovie /> },
-    { path: "/login", element: <LoginForm /> },
+
 
 ];
 
